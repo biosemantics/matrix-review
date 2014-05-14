5e4c13f0f5d080c9ce535cab02bc0db263200e01
rodenhausen <thomas.rodenhausen@gmail.com>
2014-05-14 12:46:50 -0700

    updated model according to treestore

---
+++ src/edu/arizona/biosemantics/matrixreview/client/event/MoveTaxaEvent.java
@@ -0,0 +1,53 @@
+package edu.arizona.biosemantics.matrixreview.client.event;
+
+import java.util.List;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+
+import edu.arizona.biosemantics.matrixreview.client.event.MoveTaxaEvent.MoveTaxaEventHandler;
+import edu.arizona.biosemantics.matrixreview.shared.model.Taxon;
+
+public class MoveTaxaEvent extends GwtEvent<MoveTaxaEventHandler> {
+
+	public interface MoveTaxaEventHandler extends EventHandler {
+		void onMove(MoveTaxaEvent event);
+	}
+	
+	public static Type<MoveTaxaEventHandler> TYPE = new Type<MoveTaxaEventHandler>();
+	
+	private Taxon parent;
+	private int index;
+	private List<Taxon> taxa;
+
+	public MoveTaxaEvent(Taxon parent, int index, List<Taxon> taxa) {
+		super();
+		this.parent = parent;
+		this.index = index;
+		this.taxa = taxa;
+	}
+
+	@Override
+	protected void dispatch(MoveTaxaEventHandler handler) {
+		handler.onMove(this);
+	}
+
+	@Override
+	public Type<MoveTaxaEventHandler> getAssociatedType() {
+		return TYPE;
+	}
+
+	public Taxon getParent() {
+		return parent;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public List<Taxon> getTaxa() {
+		return taxa;
+	}
+
+
+}
