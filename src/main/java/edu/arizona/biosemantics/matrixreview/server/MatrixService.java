7f3b5c9935aa6b4dd44c3d5f1a3c43670340f7f7
danveno <danveno@163.com>
2017-08-17 15:50:27 -0700

    update pom

--- src/main/java/edu/arizona/biosemantics/matrixreview/server/MatrixService.java
+++ src/main/java/edu/arizona/biosemantics/matrixreview/server/MatrixService.java
@@ -37,7 +37,7 @@ import edu.arizona.biosemantics.matrixreview.shared.model.core.Organ;
 import edu.arizona.biosemantics.matrixreview.shared.model.core.Taxon;
 import edu.arizona.biosemantics.matrixreview.shared.model.core.TaxonMatrix;
 import edu.arizona.biosemantics.matrixreview.shared.model.core.Value;
-import edu.arizona.biosemantics.common.ling.transform.IInflector;
+//import edu.arizona.biosemantics.common.ling.transform.IInflector;
 import edu.arizona.biosemantics.common.taxonomy.Rank;
 import edu.arizona.biosemantics.common.taxonomy.RankData;
 import edu.arizona.biosemantics.common.taxonomy.TaxonIdentification;
